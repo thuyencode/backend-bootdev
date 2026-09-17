3fb50ac3049107953e1553b62d937ac3c823d2ea
Thuyen Code <thuyencode@proton.me>
2026-09-17 10:13:31 +0700

    CH12-L11: Custom Package Continued

--- learn-go/hellogo/main.go
+++ learn-go/hellogo/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/thuyencode/backend-bootdev/learn-go/mystrings"
+)
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println(mystrings.Reverse("hello world"))
 }
