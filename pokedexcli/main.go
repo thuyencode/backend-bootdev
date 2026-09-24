7371cee99c9377bf197410bfca7e306e64c98155
Thuyen Code <thuyencode@proton.me>
2026-09-24 15:38:55 +0700

    CH1-L4: REPL

--- pokedexcli/main.go
+++ pokedexcli/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		if ok := scanner.Scan(); !ok {
+			if err := scanner.Err(); err != nil {
+				println("error reading user input: %w", err)
+				return
+			}
+
+			return
+		}
+
+		input := strings.ToLower(scanner.Text())
+		args := cleanInput(input)
+
+		if len(args) > 0 {
+			fmt.Printf("Your command was: %s\n", args[0])
+		}
+	}
 }
