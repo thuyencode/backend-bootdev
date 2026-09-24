771ba28b295051b69b9334d43ead186586351fda
Thuyen Code <thuyencode@proton.me>
2026-09-24 16:21:52 +0700

    CH2-L1: State

--- pokedexcli/main.go
+++ pokedexcli/main.go
@@ -9,23 +9,26 @@ import (
 
 type cliCommand struct {
 	name, description string
-	callback          func() error
+	callback          func(*cliConfig) error
+}
+
+type cliConfig struct {
+	commands map[string]cliCommand
 }
 
 func main() {
-	commands := make(map[string]cliCommand)
-	commands["exit"] = cliCommand{
-		name:        "exit",
-		description: "Exit the Pokedex",
-		callback:    commandExit,
-	}
-	commands["help"] = cliCommand{
-		name:        "help",
-		description: "Displays a help message",
-		callback: func() error {
-			return commandHelp(commands)
+	config := cliConfig{commands: map[string]cliCommand{
+		"exit": cliCommand{
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
 		},
-	}
+		"help": cliCommand{
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}}
 
 	scanner := bufio.NewScanner(os.Stdin)
 
@@ -48,35 +51,36 @@ func main() {
 			continue
 		}
 
-		command, ok := commands[args[0]]
+		command, ok := config.commands[args[0]]
 		if !ok {
 			fmt.Println("Unknown command")
 			continue
 		}
 
-		err := command.callback()
+		err := command.callback(&config)
 		if err != nil {
 			fmt.Printf("Error executing %q command: %s\n", command.name, err.Error())
 		}
 	}
 }
 
-func commandExit() error {
+func commandExit(_ *cliConfig) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 
 	return nil
 }
 
-func commandHelp(commands map[string]cliCommand) error {
-	if len(commands) == 0 {
+func commandHelp(c *cliConfig) error {
+	if len(c.commands) == 0 {
 		return fmt.Errorf("No commands registered")
 	}
 
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
+	fmt.Println()
 
-	for _, command := range commands {
+	for _, command := range c.commands {
 		fmt.Printf("%s: %s\n", command.name, command.description)
 	}
 
